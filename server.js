d71e1a35157b458afb70137ececd808d5f6ac18a
Rezz909 <ktbreza50@gmail.com>
2026-09-01 09:26:47 +0800

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        ok: true,
+        service: "Rezzz Backend",
+        message: "Backend berhasil online!"
+    });
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        ok: true,
+        service: "rezzz-backend"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Rezzz Backend berjalan di port ${PORT}`);
+});
